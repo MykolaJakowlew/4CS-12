0e942fa693434aca4947754dc3b209a9155474f9
chatbotbuilder <robotbuilder@chatbots.studio>
2022-11-10 14:27:02 +0200

    clean

--- src/App.js
+++ src/App.js
@@ -1,22 +1,9 @@
 import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import './App.css';
-import Navigation from './components/navigation';
-import Content from './components/content';
-import About from './components/about';
-import NotFoundPage from './components/404';
 
 function App () {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navigation />
-        <Routes>
-          <Route path='/' element={<Content />} />
-          <Route path='/about' element={< About />} />
-          <Route path='*' element={<NotFoundPage />} />
-        </Routes>
-      </BrowserRouter>
     </div>
   );
 }
